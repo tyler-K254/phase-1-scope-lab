112f64f6762f9512516c7d4251d6920cf24f59d6
Tyler Kirumba <Tyler>
2023-03-19 02:13:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function nameOfCustomer(){
+    return customerName
+}
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+// let bestCustomer = "bob";
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+    return(bestCustomer)
+}
+
+function overwriteBestCustomer(updatedBest){
+    bestCustomer = updatedBest
+}
+
+const leastFavouriteCustomer = "Karen"
+
+function changeLeastFavoriteCustomer(updatedLeast){
+    leastFavouriteCustomer = updatedLeast
+}
